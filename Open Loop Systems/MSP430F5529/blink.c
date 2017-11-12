ef04c0a8089840be9467826159738e615ce20272
mammarelm7 <mammarelm7@students.rowan.edu>
2017-11-12 17:41:59 -0500

    added

---
+++ Open Loop Systems/MSP430F5529/blink.c
@@ -0,0 +1,103 @@
+/*
+Matt Mammarelli
+9/18/17
+ECE 09342-2
+*/
+
+//PWM Part 2 MSP430F5529
+//Receives a duty cycle over UART, sends the pwm over GPIO pin 1.2
+
+
+
+#include <msp430f5529.h>
+
+
+int pwm=0; //holds UART values for current rgb
+
+
+void main(void)
+{
+
+  //stop watchdog timer
+  WDTCTL = WDTPW + WDTHOLD;
+
+  //uart **************************************************************************************************
+  // P3.3, P3.4 transmit/receive
+  P3SEL = BIT3+BIT4;
+  // Put state machine in reset
+  UCA0CTL1 |= UCSWRST;
+  // SMCLK
+  UCA0CTL1 |= UCSSEL_2;
+  // 1MHz 9600 baud
+  UCA0BR0 = 6;
+  // 1MHz 9600
+  UCA0BR1 = 0;
+  //sets m control register
+  UCA0MCTL = UCBRS_0 + UCBRF_13 + UCOS16;
+  //sets control register
+  UCA0CTL1 &= ~UCSWRST;
+  //enable interrupt
+  UCA0IE |= UCRXIE;
+  //*******************************************************************************************************
+
+
+  //rgb pwm *****************************************************************************************
+
+  // P1.2  output
+  P1DIR |= BIT2;
+
+  // P1.2 select GPIO
+  P1SEL |= BIT2;
+
+  // PWM Period about 1khz
+  TA0CCR0 = 1024;
+
+  // CCR1 reset/set
+  TA0CCTL1 = OUTMOD_7;
+
+
+
+  // SMCLK, up mode, clear TAR
+  TA0CTL = TASSEL_2 + MC_1 + TACLR;
+
+  //***************************************************************************************************
+
+  // Low power mode
+  __bis_SR_register(LPM0_bits + GIE);
+
+  // For debugger
+  __no_operation();
+}
+
+
+
+
+//uart interrupt vector
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+{
+  switch(__even_in_range(UCA0IV,4))
+  {
+  case 0:break;   // Vector 0 - no interrupt
+  case 2:{
+      while (!(UCA0IFG&UCTXIFG));  // USCI_A0 TX buffer check
+
+
+      pwm = UCA0RXBUF;
+      // CCR1 PWM duty cycle
+      TA0CCR1 = pwm * 4;
+
+
+      break;
+
+  }
+
+  case 4:break;    // Vector 4 - TXIFG
+  default: break;
+  }
+}
+
+
+
+
+
